4e101d16532850c958709f5f3f2d35c1b4e04bf9
Dylan Kinsella <dylan.kinsella7@gmail.com>
2024-10-25 14:36:45 -0600

    creates route/controller file structure, sets up basic route and controller files

---
+++ server/controllers/toDoController.js
@@ -0,0 +1,19 @@
+const pool = require('../db');
+
+const toDoController = {
+  async getToDos(req, res, next) {
+
+  },
+
+  async createToDo(req, res, next) {
+    
+  },
+
+  async updateToDo(req, res, next) {
+    
+  },
+
+  async deleteToDo(req, res, next) {
+    
+  }
+}
---
+++ server/routes/toDoRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const toDoController = require('../controllers/toDoController');
+
+router.get();
+
+router.post();
+
+router.put();
+
+router.delete();
+
+module.exports = router;
