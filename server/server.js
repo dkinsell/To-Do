f9d360e7532b82a8757871366c3f8f02f5f75123
Dylan Kinsella <dylan.kinsella7@gmail.com>
2024-10-29 10:04:30 -0600

    adds boilerplate db and server files, sets up basic server file structure

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
