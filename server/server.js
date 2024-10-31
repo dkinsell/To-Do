c3c8de195c8b727606ce7c3b81ea929dd69a2619
Dylan Kinsella <dylan.kinsella7@gmail.com>
2024-10-31 11:13:47 -0600

    completes logic in server, routes, and controller files for all routes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const toDoRoutes = require('./routes/toDoRoutes');
 const PORT = process.env.PORT || 8080;
+
+app.use('/', toDoRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
