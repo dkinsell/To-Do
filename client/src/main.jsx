4c341fd229088f9c0c732a602f1cb9a0d62299a0
Dylan Kinsella <dylan.kinsella7@gmail.com>
2024-11-11 11:47:24 -0700

    updates server file with cors, removes unncessary components, adds logic to ToDoApp

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import ToDoApp from './ToDoApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ToDoApp />
   </StrictMode>,
 )
